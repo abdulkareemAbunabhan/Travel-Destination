6749b0d185da05d7fdb381bd89338d05c23b8074
abdulkareemAbunabhan <zman17881@gmail.com>
2023-03-08 09:28:38 +0000

    done this lab

---
+++ src/components/header/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <>
+        <h1>Tours</h1>
+        </>
+    )
+    }
+    export default Header;
---
+++ src/components/tours/Tours.js
@@ -0,0 +1,18 @@
+const data=require("../../data/db.json")
+function Tours (){
+return(
+<>
+{data.map((item)=>{
+    return(
+        <div key={item.id}>
+    <h3>{item.name}</h3>
+    <img src={item.image} alt={item.name}></img>
+</div>
+    )
+    
+
+})}
+</>
+)
+}
+export default Tours; 
